27ce173f4553fed263ce8bda81e20be6bcf28689
frh44n <107492548+frh44n@users.noreply.github.com>
2024-06-20 02:28:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,7 @@
-from config import Config
+# config.py
+
+class Config:
+    TELEGRAM_BOT_TOKEN = '7239382363:AAHBnfK80f8YwU79E5g1LxMAreFXfrtBsbU'
+    DATABASE_URL = 'postgres://insta_reels_user:Sb11wL9MUR2ael7f4z8zWauX95kXKMwu@dpg-cppjg8dds78s73ea06i0-a.oregon-postgres.render.com/insta_reels'
+    PORT = 5000  # Example port number for Flask app
+    # Add other configuration variables as needed
