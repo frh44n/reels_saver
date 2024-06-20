65b2e4af052dcf70b3ca6e20fde7a689bf108cf0
frh44n <107492548+frh44n@users.noreply.github.com>
2024-06-20 07:25:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,6 @@
 class Config:
     TELEGRAM_BOT_TOKEN = '7239382363:AAHBnfK80f8YwU79E5g1LxMAreFXfrtBsbU'
     DATABASE_URL = 'postgres://insta_reels_user:Sb11wL9MUR2ael7f4z8zWauX95kXKMwu@dpg-cppjg8dds78s73ea06i0-a.oregon-postgres.render.com/insta_reels'
-    PORT = 5000  # Example port number for Flask app
+    PORT = 5000 # Example port number for Flask app
+    WEBHOOK_URL = 'https://reels-saver.onrender.com/7239382363:AAHBnfK80f8YwU79E5g1LxMAreFXfrtBsbU'
     # Add other configuration variables as needed
