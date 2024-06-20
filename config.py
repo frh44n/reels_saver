bcf5dd70d6d324411614e13358de07faf7bab799
frh44n <107492548+frh44n@users.noreply.github.com>
2024-06-20 15:24:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,4 +5,12 @@ class Config:
     DATABASE_URL = 'postgres://insta_reels_user:Sb11wL9MUR2ael7f4z8zWauX95kXKMwu@dpg-cppjg8dds78s73ea06i0-a.oregon-postgres.render.com/insta_reels'
     PORT = 5000 # Example port number for Flask app
     WEBHOOK_URL = 'https://reels-saver.onrender.com/7239382363:AAHBnfK80f8YwU79E5g1LxMAreFXfrtBsbU'
-    # Add other configuration variables as needed
+    AUTHORIZED_USERS = [6826870863]
+# Add other configuration variables as needed
+# config.py
+
+class Config:
+    TELEGRAM_BOT_TOKEN = 'your_telegram_bot_token_here'
+    DATABASE_URL = 'your_database_url_here'
+    PORT = 5000  # Example port number for Flask app
+      # Replace with actual authorized user IDs
